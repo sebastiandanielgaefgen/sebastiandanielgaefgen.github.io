b43b569a6a5151e427e26684b9b7213a277de77e
daniel-sebastian-gaefgen <sebastian.gaefgen@gmx.de>
2020-07-17 21:06:14 +0200

    Rundgang Video

---
+++ DE/UNR/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-_0002_panorama4png",
+      "name": "_0002_Panorama4.png",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.11136615790000093,
+        "pitch": 0,
+        "fov": 1.592602508703473
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.09310272249100393,
+          "pitch": -0.11222913668685663,
+          "title": "fantasiewald",
+          "text": "<video controls><source src= video/fantasiewald.mp4 type= video/mp4>Your browser does not support the video element.</video>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
